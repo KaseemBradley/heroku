66c04f4cf41a00fb17e423c5830398f441c032d0
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-06-03 13:15:48 -0400

    set up API

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("yer");
+require("dotenv").config();
+const express = require("express");
+const server = express();
+
+console.log(process.env.USER);
+console.log(process.env.SHELL);
+
+if (process.env.NODE_ENV === "production") {
+  console.log("means code is deployed");
+}
+
+const PORT = process.env.PORT || 5000;
+
+server.get("/api", (req, res) => {
+  res.json({ message: process.env.COHORT });
+});
+
+server.listen(PORT, () => {
+  console.log("listening on port", PORT);
+});
