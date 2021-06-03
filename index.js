e82c9bf034b71bae4341de4ab4f2ac4ae33f8141
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-06-03 14:00:16 -0400

    added react app

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
 const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(express.static(path.join(__dirname, "client/build")));
 
 console.log(process.env.USER);
 console.log(process.env.SHELL);
@@ -10,6 +17,10 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const PORT = process.env.PORT || 5000;
+
+server.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 server.get("/api", (req, res) => {
   res.json({ message: process.env.COHORT });
